6553ccab7537fd94728f15e869796e8152e3f319
EuGenio00 <eugeniotchanjongombe10@gmail.com>
2023-10-19 17:52:45 -0300

    estendendo o comp Landing

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -4,8 +4,27 @@ const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page</h1>
-      <h4>Banner</h4>
-      <p>Textos</p>
+      <h3>Banner</h3>
+      <h4>Cards</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        officia, quisquam unde ab sapiente similique ad cumque asperiores
+        commodi laboriosam facilis doloremque tempore optio fuga velit nisi
+        dolores ducimus. Earum. Delectus deleniti, quos est inventore libero
+        magni laborum. Fugit dolorem laudantium in nisi minus, alias accusantium
+        natus veniam enim ea, corporis illo maiores delectus magnam
+        exercitationem itaque iure quaerat repellendus? Expedita beatae natus,
+        delectus ad tempora asperiores perspiciatis voluptates aperiam
+        consectetur at eum similique, iste inventore maxime. Pariatur, tenetur
+        consequuntur doloremque facilis suscipit obcaecati amet aliquid
+        aspernatur quam non minima. Consequuntur dolorem quaerat a cumque ullam
+        expedita? Quam voluptate delectus saepe iste optio hic tempore quia
+        debitis voluptatem obcaecati quas deserunt, quae veniam facilis amet
+        velit ipsam laboriosam mollitia. Animi. Corrupti obcaecati repellat
+        minus labore placeat incidunt quidem porro animi voluptatibus debitis
+        eum, fugit quasi quam, dolorem atque temporibus fuga est corporis
+        deleniti dolor illum accusamus quae iusto enim. Iste?
+      </p>
     </div>
   );
 };
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import "./Menu.css";
 
-<header>
-  <nav className="top-nav">
-    <Link to="/login">Login</Link>
-    <Link to="/cadastro">Cadastre-se</Link>
-  </nav>
-</header>;
+const Menu = () => {
+  return (
+    <header>
+      <nav className="top-nav">
+        <Link to="/login">Login</Link>
+        <Link to="/cadastro">Cadastre-se</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Menu;
