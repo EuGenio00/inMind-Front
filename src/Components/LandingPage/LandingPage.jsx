77dfb72b8ccee05580b1c443f784c3d8e5b85378
EuGenio00 <eugeniotchanjongombe10@gmail.com>
2023-11-01 19:07:12 -0300

    estilizando o corpo da LP

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -2,17 +2,36 @@ import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <h3>Banner</h3>
-      <h4>Cards</h4>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt
-        sapiente corporis eligendi cupiditate labore fugit praesentium at
-        blanditiis possimus explicabo, hic repellat fuga laudantium placeat eum
-        quae facere repudiandae?
-      </p>
-    </div>
+    <section>
+      <div className="landing-container">
+        {/* <h3>- Conheça nossos benefícios -</h3> */}
+        <div className="landing-descricao">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem,
+            eligendi accusantium asperiores sint, molestiae quisquam dignissimos
+            veniam harum numquam est eaque distinctio aut cumque. Deleniti
+            cupiditate soluta odit voluptatibus omnis. Et, accusantium libero
+            repellat vitae sint harum laborum neque. A nostrum nesciunt, vero
+            voluptatibus voluptates aliquam vitae odit facilis excepturi dolorem
+            est! Error sit, vel eaque reiciendis nobis repellendus voluptate?
+            Architecto fugit vel repellat modi ipsam eligendi eos non aliquam
+            deserunt quisquam magnam, accusantium corrupti iure recusandae
+            totam, et, asperiores ab perspiciatis. Delectus aliquid fuga vitae
+            saepe, iusto dolorem voluptatibus. Magni eligendi dolorem veritatis
+            corporis nostrum, nesciunt neque voluptatum? Aperiam dignissimos
+            necessitatibus temporibus reiciendis neque cum repudiandae eveniet
+            laborum! Nihil harum asperiores iste, repellendus omnis quaerat ex
+            excepturi vitae nulla.
+          </p>
+          <div className="foto-destaque">
+            <img
+              src="https://plus.unsplash.com/premium_photo-1672292536663-9385abe50163?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Foto destaque"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
