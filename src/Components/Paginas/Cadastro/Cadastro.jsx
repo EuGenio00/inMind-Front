9b5247bd9a8a8eb536c6138acac3f0ece43aea52
EuGenio00 <eugeniotchanjongombe10@gmail.com>
2023-11-05 16:19:58 -0300

    estilizacao pagina cadastro feita 100%

--- src/Components/Paginas/Cadastro/Cadastro.jsx
+++ src/Components/Paginas/Cadastro/Cadastro.jsx
@@ -1,36 +1,39 @@
 import { Link } from "react-router-dom";
 import "./Cadastro.css";
-import Footer from "../../Footer/Footer";
+// import Footer from "../../Footer/Footer";
+import Botao from "../../Button/Button";
 
 const Cadastro = () => {
   return (
     <main>
-      <div className="cadastroPage-container">
-        <div className="formulario-content">
-          <h2>
-            Cadastre-se <span>JÁ</span>!
-          </h2>
-          <form action="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-            amet vero porro magni recusandae illo, omnis et suscipit odio
-            maxime? Enim facilis, quis reiciendis repellat hic dignissimos quo
-            obcaecati itaque.
-          </form>
-          <p>
-            Já tem cadastro? Faça seu login{" "}
-            <span>
-              <Link to="/login">aqui.</Link>
-            </span>
-          </p>
+      <section className="cadastroPage-container">
+        <div className="cadastroPage-content">
+          <div className="formulario-container">
+            <h2>
+              Cadastre-se <span>JÁ</span>!
+            </h2>
+            <form action="">
+              <input type="text" placeholder="Seu nome completo: " />
+              <input type="email" placeholder="E-mail: " />
+              <input type="password" placeholder="*Senha: " />
+              <Botao textoBotao="Criar conta" />
+            </form>
+            <p>
+              Já tem cadastro? Faça seu login{" "}
+              <span>
+                <Link to="/login">aqui.</Link>
+              </span>
+            </p>
+          </div>
+          <div className="logoPrincipal-content">
+            <img
+              src="public/images/LogoPrincipal.svg"
+              alt="Foto da Logo principal"
+            />
+          </div>
         </div>
-        <div className="logoPrincipal-content">
-          <img
-            src="public/images/LogoPrincipal.svg"
-            alt="Foto da Logo principal"
-          />
-        </div>
-      </div>
-      <Footer />
+      </section>
+      {/* <Footer /> */}
     </main>
   );
 };
