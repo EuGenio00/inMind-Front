0916bb129c054967bc0c77c897cccd1fcdd40c1e
Adilson Coragem <adilson.coragem@penseavanti.com.br>
2023-11-27 16:45:09 -0300

    atualizando as integrações

--- src/Components/Paginas/PageScheduling/PageScheduling.jsx
+++ src/Components/Paginas/PageScheduling/PageScheduling.jsx
@@ -1,58 +1,75 @@
-
-import axios from "axios";
 import './PageScheduling.css'
+import { useState, useEffect } from 'react';
 
 const Scheduling = () => {
 
-
-    const Getscheduled = async () => {
-        try {    
-          const response = await axios.get('Nosso Endepoint');
-    
-          response.data.success
-
-        } catch (error) {
-          console.error('Erro ao receber os dados:', error);
+  const [dados, setDados] = useState ({
+    nome: '',
+    dataTime: '',
+    imagem: ''
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch ('http://localhost:');
+        
+        if (!response.ok) {
+          throw new Error('Algo deu errado com o Get')
         }
-      };
 
-      console.log(Getscheduled)
+        const data = await response.json();
+
+        setDados({
+          nome: data.nome,
+          dataTime: data.dataTime,
+          imagem: data.img
+        });
+      } catch (error) {
+        console.error('Erro ao obter os dados (na requisiçãp):', error.message);
+      }
+    }
+
+    fetchData();
+  
+  }, [])
+
 
 
     return (
-        <div className="container-general">
+      <div className="container-general">
 
-                <span className="out-arrow">
-                    <a href="/"><img src="src/assets/seta-esquerda.png" alt="seta" /></a>
+          <span className="out-arrow">
+              <a href="/"><img src="src/assets/seta-esquerda.png" alt="seta" /></a>
+          </span>
+          <ul className="research-list">
+            <li className="Scheduling">
+              <div className="card-research">
+                <span className="informations">
+                  <label htmlFor="nome">{dados.nome}</label>
+                  <input 
+                    type="text" 
+                    name="nome"
+                    placeholder="Nome"
+                    value=''
+                    onChange=''/>
+                    <label htmlFor="date">{dados.dataTime}</label>
+                    <input 
+                      type="text"
+                      name="descricao"
+                      placeholder="dd/mm/aaaa - 00:00"
+                      value=""
+                      onChange=""
+                    
+                    />
                 </span>
-              <ul className="research-list">
-                <li className="Scheduling">
-                    <div className="card-research">
-                      <span className="informations">
-                        <input 
-                          type="text" 
-                          name="nome"
-                          placeholder="Nome"
-                          value=''
-                          onChange=''/>
-                          <input 
-                            type="text"
-                            name="descricao"
-                            placeholder="dd/mm/aaaa - 00:00"
-                            value=""
-                            onChange=""
-                          
-                          />
-                      </span>
-                      <div className="photo">
-                        <img src="" alt="foto" />
-                      </div>
-                    </div>
-                </li>
-              </ul>
-        </div>
-        
-        )
+                <div className="photo">
+                  <img src={dados.imagem} alt="foto" />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div> 
+      )
 
 }
 
